efddd885bd9e973a331f43d28e3e5c38520b2dfd
Krzysztof <krisaczi@yahoo.com>
2024-07-18 08:01:09 +0200

    button changed

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
                 onClick={() => handleGetStarted()}
                 className="get-started-btn"
               >
-                Get Started
+                Book your conference
               </button>
             </div>
           </div>
